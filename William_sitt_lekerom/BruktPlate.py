f19eac628109bc3c8428bc6bc1b1e2614f85e93e
williamvin <wvinding@gmail.com>
2021-06-30 10:09:06 +0200

    # Fjernet en vindu

--- William_sitt_lekerom/BruktPlate.py
+++ William_sitt_lekerom/BruktPlate.py
@@ -18,7 +18,7 @@ while True:
     frame_gray = cv2.cvtColor(effect, cv2.COLOR_BGR2GRAY)
     ret, thresh = cv2.threshold(frame_gray, 127, 255, 0)
     contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    cv2.drawContours(frame_gray, contours, -1, (0, 255, 0), 3)
+    cv2.drawContours(effect, contours, -1, (0, 255, 0), 3)
 
     cv2.putText(effect, 'B:{},C:{}'.format(brightness,
                                            contrast),
@@ -26,7 +26,7 @@ while True:
                 0.5, (0, 0, 255), 1)
 
     cv2.imshow("effect", effect)
-    cv2.imshow("frame gray", frame_gray)
+    # cv2.imshow("frame gray", frame_gray)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
