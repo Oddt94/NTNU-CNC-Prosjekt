b6b61cad9456022fa2d67e7dcbfb1b781be3b718
Odd Arne <oddisch_94@hotmail.com>
2021-07-05 10:28:29 +0200

    Main now contains contour detection

--- William_sitt_lekerom/BruktPlate.py
+++ William_sitt_lekerom/BruktPlate.py
@@ -30,5 +30,5 @@ while True:
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-cap.release()
+#cap.release()
 cv2.destroyAllWindows()
--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 import cv2
 
-img = cv2.imread("William_sitt_lekerom/shapes.png", 1)
+image = cv2.imread("William_sitt_lekerom/CutPlate.jpg", 1)
 
-while True:
-    cv2.namedWindow('image', cv2.WINDOW_NORMAL)
-    cv2.imshow("image", img)
-    if cv2.waitKey(1) & 0xFF == ord('q'):  # 1ms
-        break
+# convert the image to grayscale format
+img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+# apply binary thresholding
+
+ret, thresh = cv2.threshold(img_gray, 150, 255, cv2.THRESH_BINARY)
+
+# visualize the binary image
+cv2.imshow('Binary image', thresh)
+cv2.waitKey(0)
+cv2.imwrite('image_thres1.jpg', thresh)
+
+# detect the contours on the binary image using cv2.CHAIN_APPROX_NONE
+contours, hierarchy = cv2.findContours(image=thresh, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_NONE)
+
+# draw contours on the original image
+image_copy = image.copy()
+cv2.drawContours(image=image_copy, contours=contours, contourIdx=-1, color=(0, 255, 0), thickness=2,
+                 lineType=cv2.LINE_AA)
+
+# see the results
+cv2.imshow('None approximation', image_copy)
+cv2.waitKey(0)
+cv2.imwrite('contours_none_image1.jpg', image_copy)
 
 cv2.destroyAllWindows()
