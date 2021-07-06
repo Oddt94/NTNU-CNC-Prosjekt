d33405e069e95d3ba1660330732cba57d65d3d1a
Odd Arne <oddisch_94@hotmail.com>
2021-07-06 11:20:47 +0200

    Testing contour export

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import cv2
+import matplotlib.pyplot as plt
+import numpy as np
 
-image = cv2.imread("William_sitt_lekerom/CutPlate.jpg", 1)
+image = cv2.imread("William_sitt_lekerom/shapes.png", 1)
 
 # convert the image to grayscale format
 img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-# apply binary thresholding
 
+# apply binary thresholding
 ret, thresh = cv2.threshold(img_gray, 150, 255, cv2.THRESH_BINARY)
 
 # visualize the binary image
@@ -26,4 +28,42 @@ cv2.imshow('None approximation', image_copy)
 cv2.waitKey(0)
 cv2.imwrite('contours_none_image1.jpg', image_copy)
 
+# Make the coordinate arrays
+cx_data = []
+cy_data = []
+x = []
+y = []
+
+# this for loop iterates through all the contour elements found by openCV
+for cnt in contours:
+    M = cv2.moments(cnt)
+    # Finds the centers of all contour objects
+    if M['m00'] is None or M['m00'] == 0:
+        cx = 1
+        cy = 1
+    else:
+        cx = int(M['m10'] / M['m00'])
+        cy = int(M['m01'] / M['m00'])
+    cx_data.append(cx)
+    cy_data.append(cy)
+    # Separates out all he x and y coordinates of the contour edges
+    for i in range(len(cnt)):
+        x.append(cnt[i][0][0])
+        y.append(cnt[i][0][1])
 cv2.destroyAllWindows()
+
+# Starts plots to visualize the lists
+plt.figure()
+
+# Sets the coordinates such that all contours found are displayed
+plt.axis([0, np.max(x), 0, np.max(y)])
+
+# Flips the y-axis to match the openCV data format
+ax = plt.gca()
+ax.set_ylim(ax.get_ylim()[::-1])
+
+# Plots the centers of found objects in green and edges in red
+plt.plot(cx_data, cy_data, 'go')
+plt.plot(x, y, 'rx')
+plt.show()
+
