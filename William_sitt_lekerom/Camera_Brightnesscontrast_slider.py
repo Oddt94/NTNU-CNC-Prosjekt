525b652a84981181baec67674ab572acd5dd3697
william <wvinding@gmail.com>
2021-07-27 10:04:39 +0200

    Slider og BrightnessContrast.

--- William_sitt_lekerom/Camera_Brightnesscontrast_slider.py
+++ William_sitt_lekerom/Camera_Brightnesscontrast_slider.py
@@ -1,30 +1,88 @@
-import numpy as np
 import cv2
-
-cap = cv2.VideoCapture("earth.mp4")
-# cap = cv2.VideoCapture(0)
-
+import matplotlib.pyplot as plt
+import numpy as np
 
 contrast = 1.25
 brightness = 50
 
-while True:
-    ret, frame = cap.read()
-    cv2.imshow("Original", frame)
-    effect = frame.copy()
+cap = cv2.VideoCapture(0)
+ret, frame = cap.read()
 
-    effect = cv2.cvtColor(effect, cv2.COLOR_BGR2HSV)
-    effect[:,:,2] = np.clip(contrast * effect[:,:,2] + brightness, 0, 255)
-    effect = cv2.cvtColor(effect, cv2.COLOR_HSV2BGR)
+effect = frame.copy()
+effect = cv2.cvtColor(effect, cv2.COLOR_BGR2HSV)
+effect[:,:,2] = np.clip(contrast * effect[:,:,2] + brightness, 0, 255)
+effect = cv2.cvtColor(effect, cv2.COLOR_HSV2BGR)
 
-    cv2.putText(effect, 'B:{},C:{}'.format(brightness,
-                                        contrast),
-                (10, 20), cv2.FONT_HERSHEY_SIMPLEX,
-                0.5, (0, 0, 255), 1)
+cv2.imshow('Orginal', frame)
+cv2.imshow('Manipulated image', effect)
+cv2.waitKey(0)
 
-    cv2.imshow("effect",effect)
+# convert the image to grayscale format
+img_gray = cv2.cvtColor(effect, cv2.COLOR_BGR2GRAY)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-cap.release()
+# apply binary thresholding
+ret, thresh = cv2.threshold(img_gray, 150, 255, cv2.THRESH_BINARY)
+
+# visualize the binary image
+cv2.imshow('Binary image', thresh)
+cv2.waitKey(0)
+
+# detect the contours on the binary image using cv2.CHAIN_APPROX_NONE
+contours, hierarchy = cv2.findContours(image=thresh, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_NONE)
+
+# draw contours on the original image
+image_copy = image.copy()
+
+cv2.drawContours(image=image_copy, contours=contours, contourIdx=-1, color=(0, 255, 0), thickness=2,
+                 lineType=cv2.LINE_AA)
+
+# see the results
+cv2.imshow('None approximation', image_copy)
+cv2.waitKey(0)
+
+# Make the coordinate arrays
+cx_data = []
+cy_data = []
+x = []
+y = []
+
+# this for loop iterates through all the contour elements found by openCV
+for cnt in contours:
+    M = cv2.moments(cnt)
+    # Finds the centers of all contour objects
+    if M['m00'] is None or M['m00'] == 0:
+        cx = 1
+        cy = 1
+    else:
+        cx = int(M['m10'] / M['m00'])
+        cy = int(M['m01'] / M['m00'])
+    cx_data.append(cx)
+    cy_data.append(cy)
+    # Separates out all the x and y coordinates of the contour edges
+    for i in range(len(cnt)):
+        x.append(cnt[i][0][0])
+        y.append(cnt[i][0][1])
+cv2.destroyAllWindows()
+
+# Starts plots to visualize the lists
+plt.figure()
+
+# Sets the coordinates such that all contours found are displayed
+plt.axis([0, np.max(x), 0, np.max(y)])
+
+# Flips the y-axis to match the openCV data format
+ax = plt.gca()
+ax.set_ylim(ax.get_ylim()[::-1])
+
+# Plots the centers of found objects in green and edges in red
+plt.plot(cx_data, cy_data, 'go')
+plt.plot(x, y, 'rx')
+plt.show()
+
+blank_image = np.zeros((np.max(y), np.max(x), 3), np.uint8)
+cv2.drawContours(image=blank_image, contours=contours, contourIdx=-1, color=(0, 255, 0), thickness=2,
+                 lineType=cv2.LINE_AA)
+cv2.imshow('only edge', blank_image)
+cv2.imwrite('contours_black_backdrop.jpg', blank_image)
+cv2.waitKey(0)
 cv2.destroyAllWindows()
--- William_sitt_lekerom/Slider_Test_RGB.py
+++ William_sitt_lekerom/Slider_Test_RGB.py
@@ -8,35 +8,35 @@ def make_slid(a_min: int, a_max: int, curr: int, slider_id: str, root_win_name: 
     return slider_id
 
 
-def getslid(*ids, root_wind: str):
+def getslid(*ids, window: str):
     try:
-        results = [cv2.getTrackbarPos(idd, root_wind) for idd in ids]
+        results = [cv2.getTrackbarPos(idd, window) for idd in ids]
         return results[0] if len(results) == 1 else results
     except Exception as e:
         print(f'Error in getting slider value - {str(e)}')
         return None
 
 
-def setslid(id_: str, value: int, root_wind: str):
-    cv2.setTrackbarPos(id_, root_wind, value)
+def setslid(id_: str, value: int, window: str):
+    cv2.setTrackbarPos(id_, window, value)
 
 
 # Name your window
-root_wind = 'main'
-cv2.namedWindow(root_wind)
+window = 'main'
+cv2.namedWindow(window)
 
 sliders = [
-    make_slid(0, 255, 20, 'r', root_wind),
-    make_slid(0, 255, 50, 'g', root_wind),
-    make_slid(0, 255, 120, 'b', root_wind)
+    make_slid(0, 255, 20, 'r', window),
+    make_slid(0, 255, 50, 'g', window),
+    make_slid(0, 255, 120, 'b', window)
 ]
 
 # Create a black image
 img = np.ones((512, 512, 3), np.uint8)
 while True:
 
-    img[:, :] = getslid('r', 'g', 'b', root_wind=root_wind)
-    cv2.imshow(root_wind, img)
+    img[:, :] = getslid('r', 'g', 'b', window=window)
+    cv2.imshow(window, img)
 
     code = cv2.waitKey(1)
 
@@ -44,12 +44,12 @@ while True:
         break
 
     if code == ord('w'):
-        r, g, b = getslid('r', 'g', 'b', root_wind=root_wind)
+        r, g, b = getslid('r', 'g', 'b', window=window)
         print(r, g, b)
 
     if code == ord('e'):
-        setslid('r', 10, root_wind=root_wind)
-        setslid('g', 10, root_wind=root_wind)
-        setslid('b', 10, root_wind=root_wind)
+        setslid('r', 10, window=window)
+        setslid('g', 10, window=window)
+        setslid('b', 10, window=window)
 
 cv2.destroyAllWindows()
