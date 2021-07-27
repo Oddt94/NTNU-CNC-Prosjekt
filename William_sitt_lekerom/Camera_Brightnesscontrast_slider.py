3e5a8833773047a2bd4dc2fc4c6dca9a1e52bde9
william <wvinding@gmail.com>
2021-07-27 13:06:24 +0200

    Laga til en fungerende slider til brightness og contrast

--- William_sitt_lekerom/Camera_Brightnesscontrast_slider.py
+++ William_sitt_lekerom/Camera_Brightnesscontrast_slider.py
@@ -9,30 +9,24 @@ def empty(a):
     pass
 
 
-
 cv2.namedWindow("Parameters")
-cv2.resizeWindow("Parameters",640,240)
-cv2.createTrackbar("Threshold1","Parameters",220,1000,empty)
-cv2.createTrackbar("Threshold2","Parameters",5000,10000,empty)
-
+cv2.resizeWindow("Parameters", 640, 240)
+cv2.createTrackbar("Contrast", "Parameters", 43, 1000, empty)
+cv2.createTrackbar("Brightness", "Parameters", 1376, 10000, empty)
 
 while True:
     ret, frame = cap.read()
     effect = frame.copy()
 
-    contrast = 1.25
-    brightness = 50
-
-    threshold1 = float (cv2.getTrackbarPos("Threshold1", "Parameters") / 100)
-    threshold2 = float (cv2.getTrackbarPos("Threshold2", "Parameters") / 100)
+    Contrast = float(cv2.getTrackbarPos("Contrast", "Parameters") / 100)
+    Brightness = float(cv2.getTrackbarPos("Brightness", "Parameters") / 100)
 
     effect = cv2.cvtColor(effect, cv2.COLOR_BGR2HSV)
-    effect[:,:,2] = np.clip(threshold1 * effect[:,:,2] + threshold2, 0, 255)
+    effect[:, :, 2] = np.clip(Contrast * effect[:, :, 2] + Brightness, 0, 255)
     effect = cv2.cvtColor(effect, cv2.COLOR_HSV2BGR)
 
-
     imghorz = np.hstack((frame, effect))
-    cv2.imshow("Results",imghorz)
+    cv2.imshow("Results", imghorz)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 cap.release()
