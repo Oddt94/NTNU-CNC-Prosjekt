4e1942c6609e5c7058ed670963fece4b7fe65806
Vinjar Vederhus <vinjar97@gmail.com>
2021-07-28 15:47:06 +0200

    Pause-funksjon

--- main_v2.py
+++ main_v2.py
@@ -1,9 +1,12 @@
 import cv2
 import numpy as np
+from numpy.lib.function_base import _percentile_dispatcher
 from Stacking_func import stackImages
 
 frameWidth = 640
 frameHeight = 480
+
+paused = False
 
 cap = cv2.VideoCapture(0)
 if not cap.isOpened():
@@ -16,17 +19,24 @@ cap.set(4, frameHeight)
 def empty(a):
     pass
 
+def pauseFunc(a):
+    global paused
+    paused = not paused
+
 cv2.namedWindow("Output", flags=cv2.WINDOW_AUTOSIZE)
 cv2.createTrackbar("Contrast", "Output", 100, 1000, empty)
 cv2.createTrackbar("Brightness", "Output", 1400, 10000, empty)
 cv2.createTrackbar("Threshold1", "Output", 155, 255, empty)
+cv2.createTrackbar("Pause", "Output", 0, 1, pauseFunc)
 
 
 # Using 'value' pointer is unsafe and deprecated. Use NULL as value pointer. To fetch trackbar value setup callback
 # This is a bug from OpenCV, pay no attention to it.
 
 while True:
-    ret, frame = cap.read()
+    if(not paused):
+        ret, frame = cap.read()
+    
     Contrast = float(cv2.getTrackbarPos("Contrast", "Output") / 100)
     Brightness = float(cv2.getTrackbarPos("Brightness", "Output") / 100)
     Threshold_1 = cv2.getTrackbarPos("Threshold1", "Output")
@@ -47,19 +57,20 @@ while True:
     blank_image = np.zeros((frameHeight, frameWidth, 3), np.uint8)
 
     cv2.drawContours(image=image_copy, contours=contours, contourIdx=-1, color=(0, 255, 0), thickness=2,
-                     lineType=cv2.LINE_AA)
+                    lineType=cv2.LINE_AA)
 
     # blank_image = np.zeros(frame.shape)
     cv2.drawContours(image=blank_image, contours=contours, contourIdx=-1, color=(0, 255, 0), thickness=2,
-                     lineType=cv2.LINE_AA)
+                    lineType=cv2.LINE_AA)
 
     imgStack = stackImages(0.65, ([frame, blank_image],
-                                 [thresh, image_copy]))
+                                [thresh, image_copy]))
     cv2.imshow("Output", imgStack)
 
     if cv2.waitKey(1) & 0xFF == ord('s'):
         cv2.imwrite("Saved_img/main_v2.png", imgStack)
     elif cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
 cap.release()
 cv2.destroyAllWindows()
