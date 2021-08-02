b8bac9128607b7f48b848d62010fb38377f5cb08
william <wvinding@gmail.com>
2021-08-02 16:47:04 +0200

    Lage ferdig output av CAD i edxf

--- William_sitt_lekerom/ezdxf_file_format.py
+++ William_sitt_lekerom/ezdxf_file_format.py
@@ -5,41 +5,33 @@ doc = ezdxf.new(dxfversion='R2010')
 doc.layers.new('TEXTLAYER', dxfattribs={'color': 2})
 msp = doc.modelspace()
 
-point_list_start = 0
-point_list = [(155, 53), (462, 500), (769, 52)]
-
 # point_file = open("main_copytst2_contours.txt", "r")
 
-with open("main_copytst2_contours.txt","r") as point_file:
+with open("main_copytst3_contours.txt", "r") as point_file:
     filter = point_file.read()
     filter = filter.replace(" ", "").replace("\n\n", "\n")
-    filter = filter.replace("array([" , "").replace("dtype" , "")
+    filter = filter.replace("array([", "").replace("dtype", "")
     filter = filter.replace("int32", "").replace(",=)", "")
-    filter = filter.replace("[[", "").replace("]],\n",",").replace("]]]","")
+    filter = filter.replace("[[", "").replace("]],\n", ",").replace("]]]", "")
 
-lines_list  = filter.split(",")
+lines_list = filter.split(",")
 point_list2 = ([int(x) for x in lines_list])
-
-
-
-
-counter_stop = (len(point_list) - 1)
-point_a = -1
-point_b = 0
+counter_stop = (len(point_list2) / 2)
+counter = 1
+pl2 = point_list2
+i = 0
 
 while True:
-    point_a = point_a + 1
-    point_b = point_b + 1
-    # print(point_list[point_a], point_list[point_b])
-    msp.add_line(point_list[point_a], point_list[point_b], line_color)
-    if point_b == counter_stop:
-        # print(point_list[point_b], point_list[0])
-        msp.add_line(point_list[point_b], point_list[0], line_color)
+    counter = counter + 1
+    point_a = (pl2[i+0], pl2[i+1])
+    point_b = (pl2[i+2], pl2[i+3])
+    msp.add_line(point_a, point_b, line_color)
+    i = i + 2
+    if counter >= counter_stop:
+        point_a = (pl2[i], pl2[i+1])
+        point_b = (pl2[i-i], pl2[i-i-1])
+        msp.add_line(point_a, point_b, line_color)
         break
-
-# msp.add_line(point_0, point_1, line_color)
-# msp.add_line(point_1, point_2, line_color)
-# msp.add_line(point_2, point_0, line_color)
 msp.add_text(
     'Table Test',
     dxfattribs={
