1bd7f36b3f37f98426d544d552d3a0b973962abc
Odd Arne <oddisch_94@hotmail.com>
2021-08-03 12:54:55 +0200

    Fixed name

--- Contour_to_SVG.py
+++ Contour_to_SVG.py
@@ -1,6 +1,38 @@
 import cv2
 import numpy as np
-from functions.Stacking_func import stackImages
+import svgwrite as svg
+
+# noinspection PyPep8Naming
+def stackImages(scale, imgArray):
+    rows = len(imgArray)
+    cols = len(imgArray[0])
+    rowsAvailable = isinstance(imgArray[0], list)
+    width = imgArray[0][0].shape[1]
+    height = imgArray[0][0].shape[0]
+    if rowsAvailable:
+        for x in range ( 0, rows):
+            for y in range(0, cols):
+                if imgArray[x][y].shape[:2] == imgArray[0][0].shape [:2]:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
+                else:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]), None, scale, scale)
+                if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv2.cvtColor( imgArray[x][y], cv2.COLOR_GRAY2BGR)
+        imageBlank = np.zeros((height, width, 3), np.uint8)
+        hor = [imageBlank]*rows
+        hor_con = [imageBlank]*rows
+        for x in range(0, rows):
+            hor[x] = np.hstack(imgArray[x])
+        ver = np.vstack(hor)
+    else:
+        for x in range(0, rows):
+            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
+                imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
+            else:
+                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None,scale, scale)
+            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
+        hor= np.hstack(imgArray)
+        ver = hor
+    return ver
 
 frameWidth = 640
 frameHeight = 480
@@ -29,7 +61,8 @@ while True:
     # If not paused - get next
     if not paused:
         ret, frame = cap.read()
-
+    #for tape 6.10, 500, 128
+    # for plate 1.26, 500, 128
     Contrast = 6.10
     Brightness = 500
     Threshold_1 = 128
@@ -43,11 +76,16 @@ while True:
 
     ret, thresh = cv2.threshold(img_gray, Threshold_1, 255, cv2.THRESH_BINARY)
 
-    # cv2.findContours( canny_output, contours, hierarchy, Imgproc.RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
     contours, hierarchy = cv2.findContours(image=thresh, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_NONE)
 
     image_copy = effect.copy()
     blank_image = np.zeros((frameHeight, frameWidth, 3), np.uint8)
+    new_contour = []
+    for i in range(len(hierarchy[0])):
+        if hierarchy[0][i][3] != -1:
+            new_contour.append(contours[i])
+    contours = new_contour
+
 
     cv2.drawContours(image=image_copy, contours=contours, contourIdx=-1, color=(0, 255, 0), thickness=2,
                     lineType=cv2.LINE_AA)
@@ -58,7 +96,6 @@ while True:
     imgStack = stackImages(0.65, ([frame, blank_image],
                                 [thresh, image_copy]))
     cv2.imshow("Output", imgStack)
-
     key_press = cv2.waitKey(1)
     if key_press & 0xFF == ord('s'):
         # Make the coordinate arrays
@@ -85,12 +122,19 @@ while True:
                 y.append(cnt[i][0][1])
         print("Saving image...")
         cv2.imwrite("Saved_img/test_img.png", imgStack)
-        file_cont = open("./test.txt", "a")
+        file_cont = open("./test.txt", "w+")
         print("Saving Contours...")
         for i in range(len(x)):
             line = str(x[i]) + "," + str(y[i]) + "\n"
             file_cont.writelines(line)
         file_cont.close()
+        sheet = svg.Drawing('sheet.svg')
+        for cnt in contours:
+            for i in range(len(cnt)-1):
+                sheet.add(sheet.line((int(cnt[i][0][0]), int(cnt[i][0][1])), (int(cnt[i+1][0][0]), int(cnt[i+1][0][1])), stroke=svg.rgb(0, 0, 0, '%')))
+            sheet.add(sheet.line((int(cnt[0][0][0]), int(cnt[0][0][1])), (int(cnt[-1][0][0]), int(cnt[-1][0][1])), stroke=svg.rgb(0, 0, 0, '%')))
+        sheet.save()
+
         print("Quitting...")
         break
     elif key_press & 0xFF == ord('q') or key_press == 27:
