98b5a40c6cdb1338fceec785773c28370edf3b4e
Odd Arne <oddisch_94@hotmail.com>
2021-08-04 15:48:37 +0200

    UPDATED CONVERSION

--- Contour_to_SVG.py
+++ Contour_to_SVG.py
@@ -3,6 +3,7 @@ import numpy as np
 import svgwrite as svg
 from svgwrite import mm
 
+
 # noinspection PyPep8Naming
 def stackImages(scale, imgArray):
     rows = len(imgArray)
@@ -11,16 +12,17 @@ def stackImages(scale, imgArray):
     width = imgArray[0][0].shape[1]
     height = imgArray[0][0].shape[0]
     if rowsAvailable:
-        for x in range ( 0, rows):
+        for x in range(0, rows):
             for y in range(0, cols):
-                if imgArray[x][y].shape[:2] == imgArray[0][0].shape [:2]:
+                if imgArray[x][y].shape[:2] == imgArray[0][0].shape[:2]:
                     imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
                 else:
-                    imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]), None, scale, scale)
-                if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv2.cvtColor( imgArray[x][y], cv2.COLOR_GRAY2BGR)
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]),
+                                                None, scale, scale)
+                if len(imgArray[x][y].shape) == 2: imgArray[x][y] = cv2.cvtColor(imgArray[x][y], cv2.COLOR_GRAY2BGR)
         imageBlank = np.zeros((height, width, 3), np.uint8)
-        hor = [imageBlank]*rows
-        hor_con = [imageBlank]*rows
+        hor = [imageBlank] * rows
+        hor_con = [imageBlank] * rows
         for x in range(0, rows):
             hor[x] = np.hstack(imgArray[x])
         ver = np.vstack(hor)
@@ -29,27 +31,51 @@ def stackImages(scale, imgArray):
             if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
                 imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
             else:
-                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None,scale, scale)
+                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None, scale, scale)
             if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
-        hor= np.hstack(imgArray)
+        hor = np.hstack(imgArray)
         ver = hor
     return ver
+
 
 def transform_pixel_to_mm(dist_px):
     x1 = 47
     x2 = 566
     y1 = 0
     y2 = 1000
-    a = (y1- y2)/(x1-x2)
-    b = y2 - a * x2
-    dist_mm = a * dist_px + b
+    a = (y1 - y2) / (x1 - x2)
+    b = int(y2 - a * x2)
+    dist_mm = round(a * dist_px + b)
     return dist_mm
+
+
+def flip_x_axis(y_coord_cam):
+    x1 = 0
+    x2 = 1000
+    y1 = 1000
+    y2 = 0
+    a = (y1 - y2) / (x1 - x2)
+    b = y2 - a * x2
+    new_x_coord = round(a * y_coord_cam + b)
+    return new_x_coord
+
+
+def flip_y_axis(x_coord_cam):
+    x1 = 0
+    x2 = 1500
+    y1 = 0
+    y2 = 1500
+    a = (y1 - y2) / (x1 - x2)
+    b = y2 - a * x2
+    new_y_coord = round(a * x_coord_cam + b)
+    return new_y_coord
+
 
 frameWidth = 640
 frameHeight = 480
 paused = False
 calibrate = input('Do you want to calibrate y/n')
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
 if not cap.isOpened():
     print("Error opening video")
 cap.set(cv2.CAP_PROP_AUTOFOCUS, 0)
@@ -64,14 +90,13 @@ def empty(a):
 
 cv2.namedWindow("Output", flags=cv2.WINDOW_AUTOSIZE)
 
-
 # Using 'value' pointer is unsafe and deprecated. Use NULL as value pointer. To fetch trackbar value setup callback
 # This is a bug from OpenCV, pay no attention to it.
 # for tape 6.10, 500, 128
 # for plate 1.26, 500, 128
 # for cloth cover background 0.69 10.55 117
 Contrast = 1
-Brightness = 62.22
+Brightness = 75
 Threshold_1 = 171
 
 if calibrate == 'y':
@@ -104,16 +129,14 @@ while True:
     image_copy = effect.copy()
     blank_image = np.zeros((frameHeight, frameWidth, 3), np.uint8)
 
-
-
     cv2.drawContours(image=image_copy, contours=contours, contourIdx=-1, color=(0, 255, 0), thickness=2,
-                    lineType=cv2.LINE_AA)
+                     lineType=cv2.LINE_AA)
 
     cv2.drawContours(image=blank_image, contours=contours, contourIdx=-1, color=(0, 255, 0), thickness=2,
-                    lineType=cv2.LINE_AA)
+                     lineType=cv2.LINE_AA)
 
     imgStack = stackImages(0.65, ([frame, blank_image],
-                                [thresh, image_copy]))
+                                  [thresh, image_copy]))
     cv2.imshow("Output", imgStack)
     key_press = cv2.waitKey(1)
     if key_press & 0xFF == ord('s'):
@@ -154,9 +177,15 @@ while True:
         file_cont.close()
         sheet = svg.Drawing('sheet.svg')
         for cnt in contours:
-            for i in range(len(cnt)-1):
-                sheet.add(sheet.line((transform_pixel_to_mm(int(cnt[i][0][0]))*mm, transform_pixel_to_mm(int(cnt[i][0][1]))*mm), (transform_pixel_to_mm(int(cnt[i+1][0][0]))*mm, transform_pixel_to_mm((int(cnt[i+1][0][1])))*mm), stroke=svg.rgb(0, 0, 0, '%')))
-            sheet.add(sheet.line((transform_pixel_to_mm(int(cnt[0][0][0]))*mm, transform_pixel_to_mm(int(cnt[0][0][1]))*mm), (transform_pixel_to_mm(int(cnt[-1][0][0]))*mm, transform_pixel_to_mm((int(cnt[-1][0][1])))*mm), stroke=svg.rgb(0, 0, 0, '%')))
+            for i in range(len(cnt) - 1):
+                sheet.add(sheet.line(
+                    (flip_x_axis(transform_pixel_to_mm(cnt[i][0][1])), flip_y_axis(transform_pixel_to_mm(cnt[i][0][0]))), (
+                        flip_x_axis(transform_pixel_to_mm(cnt[i + 1][0][1])),
+                        flip_y_axis(transform_pixel_to_mm((cnt[i + 1][0][0])))), stroke=svg.rgb(0, 0, 0, '%')))
+            sheet.add(sheet.line(
+                (flip_x_axis(transform_pixel_to_mm(cnt[0][0][1])), flip_y_axis(transform_pixel_to_mm(cnt[0][0][0]))), (
+                    flip_x_axis(transform_pixel_to_mm(cnt[-1][0][1])),
+                    flip_y_axis(transform_pixel_to_mm((cnt[-1][0][0])))), stroke=svg.rgb(0, 0, 0, '%')))
         sheet.save()
 
 
